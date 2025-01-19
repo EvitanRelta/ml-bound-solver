54b4c89e2009c1281b324537a3f9308d79d945ec
EvitanRelta <shauntanzongzhi@gmail.com>
2025-01-19 12:21:36 +0800

    Add docstring for 'Add_SL'

--- src/modules/solver_layers/add.py
+++ src/modules/solver_layers/add.py
@@ -3,6 +3,8 @@ from typing_extensions import override
 
 
 class Add_SL(nn.Identity):
+    """The solver layer for "Add" layers."""
+
     @override
     def __init__(self) -> None:
         super().__init__()
